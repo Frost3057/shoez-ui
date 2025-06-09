641ba01043e0ff00e0bdbc62e5fbe764e34a45e3
Aman Bajpai <amanbajpai5660@gmail.com>
2025-06-09 19:48:17 +0530

    Scaffold update

--- app/src/main/java/com/example/shoez/frontend/homepage/homepage.kt
+++ app/src/main/java/com/example/shoez/frontend/homepage/homepage.kt
@@ -1,8 +1,11 @@
 package com.example.shoez.frontend.homepage
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,7 +15,17 @@ import com.example.shoez.frontend.homepage.Components.categoryTile
 
 @Composable
 fun homePage(){
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
-        Text("HomePage")
+    Scaffold (
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.background,
+        bottomBar = {
+
+        }
+    ){
+        padding->
+        Column(modifier = Modifier.fillMaxSize().padding(padding), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+
+        }
     }
+
 }
